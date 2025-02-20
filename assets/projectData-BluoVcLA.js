5e4bf7bb1a4e437fda76fe9902aec406d89a6f1c
Arista Ranka <rankaarista@Aristas-MacBook-Pro.local>
2025-02-20 00:12:16 -0600

    v1.3

---
+++ assets/projectData-BluoVcLA.js
@@ -0,0 +1,1 @@
+const i=[{id:1,title:"Project 1",description:"This is a description of project 1"},{id:2,title:"Project 1",description:"This is a description of project 1"},{id:3,title:"Project 1",description:"This is a description of project 1"},{id:4,title:"Project 1",description:"This is a description of project 1"}];export{i as default};
